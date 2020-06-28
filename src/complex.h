ad9cc7b48b83bb813b7d6a007474ff09a3a321db
RishabhAroraa <rishabharora.ns@gmail.com>
2020-06-28 22:14:18 +0530

    Initial commit

---
+++ src/complex.h
@@ -0,0 +1,24 @@
+class complex { 
+public:
+	double real;
+	double imaginary;
+
+	complex();
+	complex(const complex&);
+	complex(double);
+	complex(double, double);
+	~complex();
+}; 
+
+const complex& operator + (const complex&, const complex&);
+const complex& operator - (const complex&, const complex&);
+const complex& operator * (const complex&, const complex&);
+const complex& operator / (const complex&, const complex&);
+const complex& operator = (const complex&, const complex&);
+
+const complex& operator + (const complex&, double num);
+const complex& operator - (const complex&, double num);
+const complex& operator * (const complex&, double num);
+const complex& operator / (const complex&, double num);
+
+const complex& absol(cosnt complex&);
