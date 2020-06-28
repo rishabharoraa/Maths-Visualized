6e70b74609922f9b1399a948693f2fc0f23a564a
RishabhAroraa <rishabharora.ns@gmail.com>
2020-06-29 00:44:02 +0530

    .

--- src/complex.cpp
+++ src/complex.cpp
@@ -1,3 +1,5 @@
+#include "complex.h"
+
 complex :: complex() {
 	this->r = 0;
 	this->i = 0;
@@ -13,30 +15,46 @@ complex :: complex(double a, double b) {
 	this->i = b;
 }
 
+complex :: ~complex() { };
+
 const complex& operator + (const complex& a, const complex& b) {
-	complex res();
+	complex res;
 	res.r = a.r + b.r;
 	res.i = a.i + b.i;
 	return res;
 }
 
 const complex& operator - (const complex& a, const complex& b) {
-	complex res();
+	complex res;
 	res.r = a.r - b.r;
 	res.i = a.i - b.i;
 	return res;
 }
 
 const complex& operator * (const complex& a, const complex& b) {
-	complex res();
+	complex res;
 	res.r = (a.r*b.r) - (a.i*b.i);
 	res.i = (a.i*b.r) + (a.r*b.i);
 	return res;
 }
 
 const complex& operator / (const complex& a, const complex& b) {
-	complex res();
+	complex res;
 	res.r = ((a.r*b.r) + (a.i*b.i)) / ((b.r*b.r) + (b.i*b.i));
 	res.i = ((a.i*b.r) - (a.r*b.i)) / ((b.r*b.r) + (b.i*b.i));
+	return res;
+}
+
+const complex& operator * (const complex& a, double k) {
+	complex res;
+	res.r = a.r * k;
+	res.i = a.i * k;
+	return res;
+}
+
+const complex& operator / (const complex& a, double k) {
+	complex res;
+	res.r = a.r / k;
+	res.i = a.i / k;
 	return res;
 }
--- src/complex.h
+++ src/complex.h
@@ -1,29 +1,24 @@
 class complex { 
 public:
-	double real;
-	double imaginary;
+	double r;
+	double i;
 
 	complex();
 	complex(const complex&);
-	complex(double);
 	complex(double, double);
 	~complex();
-
-
 }; 
 
 const complex& operator + (const complex&, const complex&);
 const complex& operator - (const complex&, const complex&);
 const complex& operator * (const complex&, const complex&);
 const complex& operator / (const complex&, const complex&);
-const complex& operator = (const complex&, const complex&);
+//const complex& operator = (const complex&, const complex&);
 
-const complex& operator + (const complex&, double num);
-const complex& operator - (const complex&, double num);
 const complex& operator * (const complex&, double num);
 const complex& operator / (const complex&, double num);
 
-const complex& absol(cosnt complex&);
+const complex& absol(const complex&);
 
 double re(const complex&);
 double im(const complex&);
