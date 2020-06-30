f1047450641134545a13241f25b08f8b5d1c40f9
RishabhAroraa <rishabharora.ns@gmail.com>
2020-06-30 16:50:39 +0530

    Update barnsley-fern.cpp

--- barnsley-fern/barnsley-fern.cpp
+++ barnsley-fern/barnsley-fern.cpp
@@ -1,6 +1,9 @@
 #include <fstream>
 #include <math.h>
 
+// -2.1820 < x < 2.6558 
+// 0 <= y <9.9983
+
 struct point {
 	double x;
 	double y;
@@ -23,17 +26,25 @@ point f1(const point& p) {
 point f2(const point& p) {
 	point res;
 	res.x = (0.85 * p.x) + (0.04 * p.y);
-	res.y = (-0.04 * p.x) + (0.85 * p.y) + (1.6);
+	res.y = (-0.04 * p.x) + (0.85 * p.y) + 1.6;
 	return res;
 }
 
 point f3(const point& p) {
 	point res;
-	res.x = 
+	res.x = (0.2 * p.x) - (0.26 * y);
+	res.y = (0.23 * p.x) + (0.22 * p.y) + 1.6;
+	return res;
+}
+
+point f4(const point& p) {
+	point res;
+	res.x = (-0.15 * p.x) + (0.28 * p.y);
+	res.y = (0.26 * p.x) + (0.24 * p.y) + 0.44;
 }
 
 int main() {
-	bool mesh[1024][1024] = { 0 };
+	bool plot[1024][1024] = { 0 };
 
 	double p[4] = {0.01,0.85,0.07,0.07};
 
