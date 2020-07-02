8a6a583c5e780630a57bf57ed57f39d271e650a5
RishabhAroraa <rishabharora.ns@gmail.com>
2020-07-02 17:26:04 +0530

    .

--- mandelbrot-set/mandelbrot-set.cpp
+++ mandelbrot-set/mandelbrot-set.cpp
@@ -4,41 +4,49 @@
 #include "../src/complex.cpp"
 
 // z(n+1) = z^2 + c
+const int side = 100;
 
-struct rgb {
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;	
-};
+unsigned char graph[side][side] = { 0 };
 
-vector<vector<rgb>> graph(500, vector<rgb>(500));
-
-void plot(const complex& c) {
-	//int y = (int)im(c) + 500;
-	//int x = (int)re(c) + 500;
-	//std::cout << "x: " << x << " y: " << y << std::endl; 
-	//graph[y][x] = 1;
+void plot(const complex& c, unsigned char value) {
+	int y = (int)(im(c)*(side/2))+(side/2);
+	int x = (int)(re(c)*(side/2))+(side/2);
+	graph[y][x] = value;
 }
 
+
 int main() {
-	plot(z);
-	for(double y = -2; y <= 2; y = y + 0.001) {
-		for(double x = -2; x <= 2; x = x + 0.001) {
-			complex z(0,0);
+	for(double y = -2; y < 2; y = y + 0.1) {
+		for(double x = -2; x < 2; x = x + 0.1) {
 			complex c(x,y);
-			bool flag = 0;
-			while(true) {
-				int iters = 0;
-				if(distanceFromOrigin(z) < 2 && iters < 20) {
-					z = (z*z) + c;
-				}
-				else {
-					flag = 1;
-					break;
-				}
-				iters++;
+			if(distanceFromOrigin(c) <= 2) {
+				plot(c,1);
+				//if(distanceFromOrigin((z*z) + c) <= 2) {
+				//	z = (z*z) + c;
+				//	plot(c,2);
+				//}
 			}
-			if(flag == 1) {}
+			c.~complex();
+		}
+	}
+	std::ofstream img ("test.ppm");
+	img << "P3" << std::endl;
+	img << side << ' ' << side << std::endl;
+	img << "255" << std::endl;
+	for(int y = 0; y < side; y++) {
+		for(int x = 0; x < side; x++) {
+			if(graph[y][x] == 1) {
+				img << "255 255 255" << std::endl;
+			}
+			else if(graph[y][x] == 2) {
+				img << "0 255 0" << std::endl;
+			}
+			else if(graph[y][x] == 3) {
+				img << "0 0 255" << std::endl;
+			}
+			else {
+				img << "255 0 0" << std::endl;
+			}
 		}
 	}
 }
