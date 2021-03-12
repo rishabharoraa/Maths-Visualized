87ff40d39e9938f3d55961a1e79aeb244185d082
Rishabh Arora <43946451+RishabhAroraa@users.noreply.github.com>
2021-03-12 11:42:28 +0530

    Update main.cpp

--- burning-ship-fractal/main.cpp
+++ burning-ship-fractal/main.cpp
@@ -3,9 +3,21 @@
 #include <vector>
 #include <iostream> //remove after debugging
 
-typedef unsigned int u32
-typedef unsigned short u8
-typedef double db
+#DEFINE PREC 5
+
+typedef unsigned int u32;
+typedef unsigned char u8;
+typedef double db;
+
+db setP(db num) {
+  num = (int)(num * (pow(10,PREC)));
+  db res = (db)(n / pow(10,PREC));
+  return res;
+}
+
+u32 iterate(const std::complex<db>& zorc) {
+  
+}
 
 int main() {
   //pass
